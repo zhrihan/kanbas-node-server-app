00cb12f4251618be0e9185eedd9b4f4f1714e210
shrihan-chinta <E19CSE026@bennett.edu.in>
2024-04-15 22:58:52 -0400

    a6 server changes

--- App.js
+++ App.js
@@ -9,13 +9,13 @@ import ModuleRoutes from "./Kanbas/modules/routes.js";
 import AssignmentRoutes from './Kanbas/assignments/routes.js';
 import UserRoutes from "./Kanbas/Users/routes.js";
 import session from "express-session";
-const CONNECTION_STRING = process.env.DB_CONNECTION_STRING ||  'mongodb://127.0.0.1:27017/kanbas';
+const CONNECTION_STRING = 'mongodb://localhost:27017/kanbas';
 mongoose.connect(CONNECTION_STRING);
 const app = express();
 app.use(cors({
   credentials: true,
-  origin: 'https://a6--hilarious-phoenix-11b533.netlify.app'
-  // origin: 'http://localhost:3000'
+  // origin: 'https://a6--hilarious-phoenix-11b533.netlify.app'
+  origin: 'http://localhost:3000'
 }
 ));
 app.use(express.json());
